20e266a0719157455269efd06eca5eda291f8695
mike632t <mike632t>
2021-09-05 00:04:09 +0100

    Removed trailing spaces

--- x11-calc-font.h
+++ x11-calc-font.h
@@ -1,10 +1,10 @@
 /*
- * x11-calc-font.h - RPN (Reverse Polish) calculator simulator. 
+ * x11-calc-font.h - RPN (Reverse Polish) calculator simulator.
  *
  * Copyright(C) 2013   MT
- * 
+ *
  * Font definations and data structures.
- *      
+ *
  * This  program is free software: you can redistribute it and/or modify  it
  * under  the  terms of the GNU General Public License as published  by  the
  * Free  Software  Foundation, either version 3 of the License, or (at  your
@@ -18,7 +18,7 @@
  * You  should have received a copy of the GNU General Public License  along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * 
+ *
  * 09 Mar 14         - Initial version - MT
  * 10 Dec 18         - Defined DEC fonts if compiling on VMS - MT
  *
