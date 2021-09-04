c572252578976978e235ab121a624c59857b0516
mike632t <mike632t>
2021-09-05 00:00:09 +0100

    Removed trailing spaces

--- gcc-debug.h
+++ gcc-debug.h
@@ -1,8 +1,8 @@
 /*
- * gcc-debug.h - RPN (Reverse Polish) calculator simulator. 
+ * gcc-debug.h - RPN (Reverse Polish) calculator simulator.
  *
  * Copyright(C) 2013   MT
- * 
+ *
  * Debug macro.
  *
  * This  program is free software: you can redistribute it and/or modify it
@@ -19,13 +19,13 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * 17 Aug 13         - Initial version - MT
- * 07 Dec 18         - Updated  allow debugging to be turned on and off  by 
+ * 07 Dec 18         - Updated  allow debugging to be turned on and off  by
  *                     setting  DEBUG true or false - MT
  * 22 Aug 21         - Prints Current filename and line number - MT
  *                   - Added verbose macro - MT
  *
  */
- 
+
 /* Execute code if DEBUG is True */
 #ifndef debug /* Don't redefine macro if already defined. */
 #define debug(code) do {if(DEBUG){fprintf(stderr, "Debug\t: %s line : %d : ", \
