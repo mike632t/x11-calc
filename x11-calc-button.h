9005f1d700a07af655289925224bfa0f0b62533e
mike632t <mike632t>
2021-09-05 00:02:33 +0100

    Removed trailing spaces

--- x11-calc-button.h
+++ x11-calc-button.h
@@ -1,12 +1,12 @@
 /*
- * x11-calc-button.h - RPN (Reverse Polish) calculator simulator. 
+ * x11-calc-button.h - RPN (Reverse Polish) calculator simulator.
  *
  * Copyright(C) 2013   MT
- * 
+ *
  * Button functions.
- *      
+ *
  * Defines the routines and data structures used to draw a button.
- * 
+ *
  * This  program is free software: you can redistribute it and/or modify it
  * under  the terms of the GNU General Public License as published  by  the
  * Free  Software Foundation, either version 3 of the License, or (at  your
@@ -35,19 +35,19 @@ typedef struct { /* Calculator button structure. */
    XFontStruct* label_font; /* Pointer to shifted font. */
    XFontStruct* alternate_font; /* Pointer to alternate font. */
    int left;
-   int top; 
+   int top;
    int width;
-   int height; 
+   int height;
    int state;
    unsigned int colour;
 } obutton;
 
 obutton *h_button_pressed(obutton *h_button, int i_xpos, int i_ypos);
 
-obutton *h_button_create(int i_index, char* s_text,char* s_label ,char* s_alternate, 
-   XFontStruct *h_normal_font, XFontStruct *h_shift_font, XFontStruct *h_alternate_font, 
+obutton *h_button_create(int i_index, char* s_text,char* s_label ,char* s_alternate,
+   XFontStruct *h_normal_font, XFontStruct *h_shift_font, XFontStruct *h_alternate_font,
    int i_left, int i_top, int i_width, int i_height, int i_state,
    unsigned int i_colour);
-   
+
 int i_button_draw(Display *h_display, int x_application_window, int i_screen,obutton *h_button);
 #endif
