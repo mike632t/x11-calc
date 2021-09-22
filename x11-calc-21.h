ee1f249e89933223dffb5f3a82813a3d721d18b7
mike632t <mike632t>
2021-09-22 01:45:59 +0100

    Updated comments

--- x11-calc-21.h
+++ x11-calc-21.h
@@ -5,17 +5,17 @@
  *
  * Model specific functions.
  *
- * This  program is free software: you can redistribute it and/or modify  it
- * under  the  terms of the GNU General Public License as published  by  the
- * Free  Software  Foundation, either version 3 of the License, or (at  your
+ * This  program is free software: you can redistribute it and/or modify it
+ * under  the terms of the GNU General Public License as published  by  the
+ * Free  Software Foundation, either version 3 of the License, or (at  your
  * option) any later version.
  *
- * This  program  is  distributed in the hope that it will  be  useful,  but
- * WITHOUT   ANY   WARRANTY;   without  even   the   implied   warranty   of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * This  program  is distributed in the hope that it will  be  useful,  but
+ * WITHOUT   ANY   WARRANTY;   without even   the   implied   warranty   of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
  *
- * You  should have received a copy of the GNU General Public License  along
+ * You  should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * 09 Mar 14   0.1   - Initial version - MT
@@ -26,19 +26,19 @@
  */
 
 #ifndef TITLE
-#define TITLE        "RPN calc 21"
-#define HEIGHT       385
-#define WIDTH        201   /* Yes really! */
-#define BUTTONS      30
+#define TITLE          "RPN calc 21"
+#define HEIGHT         385
+#define WIDTH          201 /* Yes really! */
+#define BUTTONS        30
 
-#define DIGITS       12
+#define DIGITS         12
 
-#define ROM_SIZE     02000
-#define RAM_SIZE     1
-#define ROM_BANKS    1
+#define DATA_REGISTERS 1 /* Can't be zero*/
+#define ROM_SIZE       02000
+#define ROM_BANKS      1
 
-#define True         1
-#define False        0
+#define True           1
+#define False          0
 
 int i_rom [ROM_SIZE * ROM_BANKS];
 
