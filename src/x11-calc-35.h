340afbddd587593d96e53358f8f6fa86ccb30bfa
mike632t <mike632t>
2022-01-04 21:22:27 +0000

    Added a work around for bug in 'return'

--- src/x11-calc-35.h
+++ src/x11-calc-35.h
@@ -45,7 +45,7 @@
 #define KEY_GAP          3 * SCALE_WIDTH
 #define SWITCH_HEIGHT   10 * SCALE_HEIGHT
 
-#define ROM_SIZE        01400
+#define ROM_SIZE        02000
 #define MEMORY_SIZE     1
 #define ROM_BANKS       1
 
