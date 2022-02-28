d10c6c2999820e8ba8174885ec903d421f08beca
mike632t <1799044+mike632t@users.noreply.github.com>
2022-02-28 13:45:33 +0000

    Added background shading and horizontal line

--- src/x11-calc-label.h
+++ src/x11-calc-label.h
@@ -24,21 +24,20 @@
 
 typedef struct { /* Calculator label structure */
    int index;
-   char* text; /* Text */ 
+   char* text; /* Text */
    XFontStruct* text_font; /* Pointer to font */
    int left;
    int top;
    int width;
    int height;
-   int alignment;
    unsigned int colour; /* Colour */
    unsigned int background; /* Background colour */
 } olabel;
 
 olabel *h_label_pressed(olabel *h_label, int i_xpos, int i_ypos);
 
-olabel *h_label_create(int i_index, char* s_text, XFontStruct *h_normal_font,
-   int i_left, int i_top, int i_width, int i_height, int i_alignment,
+olabel *h_label_create(int i_index, char* s_text, XFontStruct *h_font,
+   int i_left, int i_top, int i_width, int i_height,
    unsigned int i_colour, unsigned int i_background);
 
 int i_label_draw(Display *h_display, int x_application_window, int i_screen, olabel *h_label);
