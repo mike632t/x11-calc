2969fc0c2bdae5473cd74d57e4497eb707be6e28
mike632t <1799044+mike632t@users.noreply.github.com>
2025-08-17 16:13:11 +0100

    Solaris (and FreeBSD) can use usleep()

--- src/gcc-wait.c
+++ src/gcc-wait.c
@@ -36,6 +36,7 @@
  * 22 Apr 24         - Moved  compiler feature macro definitions to generic
  *                     busy loop - MT
  * 11 Aug 25         - Warn if busy loop used (requires stdio.h) - MT
+ * 17 Aug 25         - Solaris (and FreeBSD) can use usleep() - MT
  *
  */
 
@@ -46,7 +47,7 @@
 
 #define  False          0
 #define  True           !(False)
-#if defined(linux) || defined(__linux__) || defined(__NetBSD__)
+#if defined(linux) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__solaris__)
 #include <unistd.h>
 #include <sys/types.h>
 #elif defined(VMS)
@@ -72,7 +73,7 @@
  */
 int i_wait(long l_delay)
 {
-#if defined(linux) || defined(__linux__) || defined(__NetBSD__)
+#if defined(linux) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__solaris__)
 return (usleep(l_delay * 1000)); /* Use usleep() function */
 #elif defined(VMS)
 float f_seconds;
